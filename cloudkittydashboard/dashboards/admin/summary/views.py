0fdab9d4da8fde3c512b55dee8c643227dad9bf3
Leonie <leonie@stackhpc.com>
2025-08-19 08:57:51 +0000

    Added grouping to rating panels
    Change-Id: Iac784f141dc24a0154d71f10f21c605738d96ff6
    Signed-off-by: Leonie <leonie@stackhpc.com>

--- cloudkittydashboard/dashboards/admin/summary/views.py
+++ cloudkittydashboard/dashboards/admin/summary/views.py
@@ -13,7 +13,6 @@
 #    under the License.
 
 from django.conf import settings
-from django.utils.translation import gettext_lazy as _
 from horizon import tables
 
 from openstack_dashboard.api import keystone as api_keystone
@@ -21,6 +20,8 @@ from openstack_dashboard.api import keystone as api_keystone
 from cloudkittydashboard.api import cloudkitty as api
 from cloudkittydashboard.dashboards.admin.summary import tables as sum_tables
 from cloudkittydashboard import utils
+
+from cloudkittydashboard import forms
 
 rate_prefix = getattr(settings,
                       'OPENSTACK_CLOUDKITTY_RATE_PREFIX', None)
@@ -35,8 +36,7 @@ class IndexView(tables.DataTableView):
     def get_data(self):
         summary = api.cloudkittyclient(
             self.request, version='2').summary.get_summary(
-                groupby=['project_id'],
-                response_format='object')
+            groupby=['project_id'], response_format='object')
 
         tenants, _ = api_keystone.tenant_list(self.request)
         tenants = {tenant.id: tenant.name for tenant in tenants}
@@ -47,40 +47,56 @@ class IndexView(tables.DataTableView):
             'project_id': 'ALL',
             'rate': total,
         })
+
         data = api.identify(data, key='project_id')
         for tenant in data:
             tenant['tenant_id'] = tenant.get('project_id')
             tenant['name'] = tenants.get(tenant.id, '-')
             tenant['rate'] = utils.formatRate(tenant['rate'],
                                               rate_prefix, rate_postfix)
-            data[-1]['name'] = 'Cloud Total'
+        data[-1]['name'] = 'Cloud Total'
         return data
 
 
 class TenantDetailsView(tables.DataTableView):
     template_name = 'admin/rating_summary/details.html'
     table_class = sum_tables.TenantSummaryTable
-    page_title = _("Script Details : {{ table.project_id }}")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['groupby_list'] = getattr(settings,
+                                          'OPENSTACK_CLOUDKITTY_GROUPBY_LIST',
+                                          ['type'])
+        return context
 
     def get_data(self):
         tenant_id = self.kwargs['project_id']
+        form = forms.CheckBoxForm(self.request.GET)
+        groupby = form.get_selected_fields()
 
         if tenant_id == 'ALL':
             summary = api.cloudkittyclient(
-                self.request, version='2').summary.get_summary(
-                groupby=['type'], response_format='object')
+                self.request, version='2'
+            ).summary.get_summary(groupby=groupby, response_format='object')
         else:
             summary = api.cloudkittyclient(
-                self.request, version='2').summary.get_summary(
-                    filters={'project_id': tenant_id},
-                    groupby=['type'], response_format='object')
+                self.request, version='2'
+            ).summary.get_summary(
+                filters={'project_id': tenant_id},
+                groupby=groupby,
+                response_format='object',
+            )
 
         data = summary.get('results')
         total = sum([r.get('rate') for r in data])
-        data.append({'type': 'TOTAL', 'rate': total})
 
-        for item in data:
-            item['rate'] = utils.formatRate(item['rate'],
-                                            rate_prefix, rate_postfix)
+        if not groupby:
+            data = [{'type': 'TOTAL', 'rate': total}]
+
+        else:
+            data.append({'type': 'TOTAL', 'rate': total})
+            for item in data:
+                item['rate'] = utils.formatRate(
+                    item['rate'], rate_prefix, rate_postfix)
 
         return data
